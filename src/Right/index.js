63ca5de5b0076e42c830656d56fbdcfcf6cd97f9
rankyangel <113102812@qq.com>
2021-01-11 16:38:50 +0800

    init

--- src/Right/index.js
+++ src/Right/index.js
@@ -1,10 +1,8 @@
+import { Tabs } from 'antd';
 import React, { useEffect } from 'react';
-import { useStore, useSet } from '../hooks';
-import { RightOutlined } from '@ant-design/icons';
+import { useSet, useStore } from '../hooks';
 import './index.css';
 import ItemSettings from './ItemSettings';
-import GlobalSettings from './GlobalSettings';
-import { Tabs } from 'antd';
 
 const { TabPane } = Tabs;
 
@@ -14,28 +12,7 @@ export default function Right() {
     showItemSettings: false,
   });
   const { selected } = useStore();
-  const { showRight, showItemSettings } = state;
-
-  const toggleRight = () => setState({ showRight: !showRight });
-
-  const ToggleIcon = () => (
-    <div
-      className="absolute top-0 left-0 pointer"
-      style={{ height: 30, width: 30, padding: '8px 0 0 8px' }}
-      onClick={toggleRight}
-    >
-      <RightOutlined className="f5" />
-    </div>
-  );
-
-  const HideRightArrow = () => (
-    <div
-      className="absolute right-0 top-0 h2 flex-center"
-      style={{ width: 40 }}
-    >
-      <ToggleIcon />
-    </div>
-  );
+  const { showItemSettings } = state;
 
   // 如果没有选中任何item，或者是选中了根节点，object、list的内部，显示placeholder
   useEffect(() => {
@@ -46,21 +23,18 @@ export default function Right() {
     }
   }, [selected]);
 
-  return showRight ? (
+  return (
     <div className="right-layout relative pl2">
-      <ToggleIcon />
       <Tabs defaultActiveKey="1" onChange={() => {}}>
         {showItemSettings && (
           <TabPane tab="组件配置" key="1">
             <ItemSettings />
           </TabPane>
         )}
-        <TabPane tab="表单配置" key={showItemSettings ? '2' : '1'}>
+        {/* <TabPane tab="表单配置" key={showItemSettings ? '2' : '1'}>
           <GlobalSettings />
-        </TabPane>
+        </TabPane> */}
       </Tabs>
     </div>
-  ) : (
-    <HideRightArrow />
   );
 }
